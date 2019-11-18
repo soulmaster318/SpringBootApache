8de6d5e11f49382f3d8f847f6bf4b40c3491d540
soulmaster318 <44565899+soulmaster318@users.noreply.github.com>
2019-11-18 09:21:41 +0700

    Add files via upload

---
+++ SpringBootJspTiles/src/main/java/hiepnguyen/sbtiles/SpringBootJspTilesApplication.java
@@ -0,0 +1,13 @@
+package hiepnguyen.sbtiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJspTilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJspTilesApplication.class, args);
+	}
+
+}
